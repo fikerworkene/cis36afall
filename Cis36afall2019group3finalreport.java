62a9e46c44931a5253b8723dd0e992310cd0ddc0
fikerworkene <54964707+fikerworkene@users.noreply.github.com>
2019-10-24 14:42:55 -0700

    Add files via upload

---
+++ Cis36afall2019group3finalreport.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.*;
+public class Cis36afall2019group3finalreport {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("a) group menber " +
+             "\n\t\t 1 Fiker workene" + 
+             "\n\t\t 2 Ying-Hsuen Lin" + 
+             "\n\t\t 3 Phuong phuong" + 
+             "\n\t\t 4 Dominiquede Bergue");   
+        System.out.println("\nb) Reading materials" +
+                         "\n\thttps://gamedevelopment.tutsplus.com/tutorials/"+
+            "introduction-to-javafx-for-game-development--cms-23835" +
+                         "\n\t\t\t\thttps://docs.oracle.com/javafx/2/api/" + 
+            "javafx/scene/canvas/GraphicsContext.html" + "\n\t\t\twww.cs.mun.ca/"
+            + "av/old/teaching/cs1710/notes/life_printout.pdf" ); 
+        System.out.println("\nc) Besides required Java code" + "\n\t-javaFx");
+        
+        System.out.println("\nd) Group Task Division" + "\n\tthe game of life "
+            + "Rules is Birth" + "\n\t\tSize of board is Square" + 
+            "\n\t\tthe Pattern is Glider");
+    }
+    }
+    
+    
+
