dbafa65ebc87cef607fd8eca2820a4ee94c15ab9
fikerworkene <54964707+fikerworkene@users.noreply.github.com>
2019-11-24 20:38:10 -0800

    Add files via upload

---
+++ Cis36aFall2019fikerwHw4Ex1.java
@@ -0,0 +1,293 @@
+
+/**
+ * Program Name: FractionFikerWHw4Ex1.java
+ * Discussion:   FractionFikerW
+ * Written By:   Fiker Workene
+ * Date:         2019/10/24
+ */
+
+
+import java.util.*;
+public class Cis36aFall2019fikerwHw4Ex1 {
+
+    public static void main(String[] arg)
+    {
+        System.out.print ("CIS 36A - Java Programming" +
+                "\nLaney College" +
+                "\nFiker Workene" +
+                "\n\nAssignment Information --" +
+                "\n  Assignment Number: Homework 04," +
+                "\n                     " +
+                "Coding Assignment -- Exercise #1" +
+                "\n Written by:        Fiker Workene" +
+                "\n Due Date:          2019/10/24");
+		
+        FractionUtilityFikerW.runMenuHw4();
+    }
+}
+
+/**PROGRAM OUTPUT
+ CIS 36A - Java Programming
+Laney College
+Fiker Workene
+
+Assignment Information --
+  Assignment Number: Homework 04,
+                     Coding Assignment -- Exercise #1
+ Written by:        Fiker Workene
+ Due Date:          2019/10/24
+
+
+*****************************
+*    MENU - Hw #4           *
+*  1. Initializing          *
+*  2. Arithmetic Operations *
+*  3. Uncommon Digit Search *
+*  4. Printing              *
+*  5. Quit                  *
+*****************************
+Select an option (use integer value only): 6
+
+Too Funny!
+
+
+*****************************
+*    MENU - Hw #4           *
+*  1. Initializing          *
+*  2. Arithmetic Operations *
+*  3. Uncommon Digit Search *
+*  4. Printing              *
+*  5. Quit                  *
+*****************************
+Select an option (use integer value only): 2
+
+Arithmetic Operations -
+
+    Not a proper call as no Fraction's are available!
+
+
+*****************************
+*    MENU - Hw #4           *
+*  1. Initializing          *
+*  2. Arithmetic Operations *
+*  3. Uncommon Digit Search *
+*  4. Printing              *
+*  5. Quit                  *
+*****************************
+Select an option (use integer value only): 1
+
+Initializing SubMENU -
+Left Num: -88
+
+Left Denom: 168
+
+Right Num: 398
+Right Denom: 824
+
+
+
+*****************************
+*    MENU - Hw #4           *
+*  1. Initializing          *
+*  2. Arithmetic Operations *
+*  3. Uncommon Digit Search *
+*  4. Printing              *
+*  5. Quit                  *
+*****************************
+Select an option (use integer value only): 4
+
+Printing SubMENU -
+
+  ********************************
+    *         PRINTING MENU        *
+    *  (leftOp, rightOp, result)   *
+    *  1. print() - Member         *
+    *  2. print() - Stand Alone    *
+    *  3. Return to Previous MENU  *
+    ********************************
+    Select an option (1, 2, or 3): 1
+
+    Printing Member Objects -
+
+    Left Fraction: 
+  {num: 11, denom: 21}
+
+    Right Fraction: 
+  {num: 199, denom: 412}
+
+    Result Fraction: 
+  {num: 0, denom: 1}
+
+
+  ********************************
+    *         PRINTING MENU        *
+    *  (leftOp, rightOp, result)   *
+    *  1. print() - Member         *
+    *  2. print() - Stand Alone    *
+    *  3. Return to Previous MENU  *
+    ********************************
+    Select an option (1, 2, or 3): 2
+
+    Printing Stand-Alone Objects -
+      Left Fraction: 
+      Num:11    Denom: 21
+      Right Fraction: 
+      Num:199    Denom: 412
+      Result:    Num:0    Denom:1
+
+  ********************************
+    *         PRINTING MENU        *
+    *  (leftOp, rightOp, result)   *
+    *  1. print() - Member         *
+    *  2. print() - Stand Alone    *
+    *  3. Return to Previous MENU  *
+    ********************************
+    Select an option (1, 2, or 3): 3
+
+    Return to Main MENU!
+
+
+*****************************
+*    MENU - Hw #4           *
+*  1. Initializing          *
+*  2. Arithmetic Operations *
+*  3. Uncommon Digit Search *
+*  4. Printing              *
+*  5. Quit                  *
+*****************************
+Select an option (use integer value only): 2
+
+Arithmetic Operatioins -
+
+    *******************************
+    *     Arithmetic SubMENU      *
+    *  1. Addition                *
+    *  2. Subtraction             *
+    *  3. Multiplication          *
+    *  4. Division                *
+    *  5. Return to Previous MENU *
+    *******************************
+    Select an option: 6
+
+    WRONG OPTION!...
+
+    *******************************
+    *     Arithmetic SubMENU      *
+    *  1. Addition                *
+    *  2. Subtraction             *
+    *  3. Multiplication          *
+    *  4. Division                *
+    *  5. Return to Previous MENU *
+    *******************************
+    Select an option: 1
+
+    Addition Options --
+
+      ********************************
+      *       Addition SubMENU       *
+      *  1. add() - Member           *
+      *  2. add() - Stand Alone      *
+      *  3. Return to Previous MENU  *
+      ********************************
+      Select an option (1, 2, or 3): 2
+
+      Calling stand-alone add() ---
+
+      Left Fraction: 
+      Num:11    Denom: 21
+      Right Fraction: 
+      Num:199    Denom: 412
+      Result:    Num:8711    Denom:8652
+
+      ********************************
+      *       Addition SubMENU       *
+      *  1. add() - Member           *
+      *  2. add() - Stand Alone      *
+      *  3. Return to Previous MENU  *
+      ********************************
+      Select an option (1, 2, or 3): 3
+
+      Return to Previous MENU!
+
+    *******************************
+    *     Arithmetic SubMENU      *
+    *  1. Addition                *
+    *  2. Subtraction             *
+    *  3. Multiplication          *
+    *  4. Division                *
+    *  5. Return to Previous MENU *
+    *******************************
+    Select an option: 5
+
+    Return to Previous MENU!
+
+
+*****************************
+*    MENU - Hw #4           *
+*  1. Initializing          *
+*  2. Arithmetic Operations *
+*  3. Uncommon Digit Search *
+*  4. Printing              *
+*  5. Quit                  *
+*****************************
+Select an option (use integer value only): 3
+
+Uncommon Digit Search -
+
+    ***********************************
+    *  Uncommon Digit Search SubMENU  *
+    *  1. Using Left Fraction         *
+    *  2. Using Right Fraction        *
+    *  3. Return to Main MENU         *
+    ***********************************
+    Select an option: 5
+
+    WRONG OPTION!...
+
+    ***********************************
+    *  Uncommon Digit Search SubMENU  *
+    *  1. Using Left Fraction         *
+    *  2. Using Right Fraction        *
+    *  3. Return to Main MENU         *
+    ***********************************
+    Select an option: 1
+
+    Using Left Fraction --    2
+
+    ***********************************
+    *  Uncommon Digit Search SubMENU  *
+    *  1. Using Left Fraction         *
+    *  2. Using Right Fraction        *
+    *  3. Return to Main MENU         *
+    ***********************************
+    Select an option: 2
+
+    Using Right Fraction --    2    4    9
+
+    ***********************************
+    *  Uncommon Digit Search SubMENU  *
+    *  1. Using Left Fraction         *
+    *  2. Using Right Fraction        *
+    *  3. Return to Main MENU         *
+    ***********************************
+    Select an option: 3
+
+    Return to Main MENU!
+
+
+*****************************
+*    MENU - Hw #4           *
+*  1. Initializing          *
+*  2. Arithmetic Operations *
+*  3. Uncommon Digit Search *
+*  4. Printing              *
+*  5. Quit                  *
+*****************************
+Select an option (use integer value only): 5
+
+Have Fun ...!
+ */
+
+/**Logic Code Output Issues
+ * No comments!
+ */
---
+++ FractionFikerW.java
@@ -0,0 +1,177 @@
+/*
+ * Program Name: FractionFikerW.java
+ * Discussion:    Functions
+ * Written By:   Fiker Workene
+ * Date:         2019/10/24
+ */
+
+import java.util.*;
+
+public class FractionFikerW 
+{
+    private int num;
+    private int denom;
+    
+    public FractionFikerW()
+    {
+        num = 0;
+        denom = 1;
+    }
+    
+    public FractionFikerW(int n) {
+        num = n < 0 ? -n : n;
+        denom = 1;
+    }
+    
+    public FractionFikerW(int n, int d)
+    {
+        int gcd = gcdBF(n, d);
+        num = ((n / gcd) < 0 ? 
+                -(n / gcd) : (n / gcd));
+        if (n == 0)
+            denom = 1;
+        else
+            denom = ((d / gcd) < 0 ? 
+                    -(d / gcd) : (d / gcd));
+    }
+    
+    public int getNum() 
+    {
+        return num;
+    }
+    
+    public int getDenom() 
+    {
+        return denom;
+    }
+    
+    public void setNum(int n) 
+    {
+    	int gcd = gcdBF(n, denom);
+        
+        num = n / gcd < 0 ? -n / gcd : n / gcd;
+        denom /= gcd;
+    }
+    
+    public void setDenom(int d) 
+    {
+    	int gcd = gcdBF(num, d);
+        
+        denom = d / gcd < 0 ? -d / gcd : d / gcd;
+        num /= gcd;
+    }
+    
+    public void update(int n, int d)
+    {
+        if ((n < 0 && d < 0) || (d < 0)) {
+            n = -n;
+            d = -d;
+        }
+        if (n == 0)
+            denom = 1;            
+        else
+            denom = d / gcdBF(n,d) ; 
+        num = n / gcdBF(n, d); 
+    }
+    
+    public void print()
+    {
+        System.out.println("\n  {num: " +
+                num + ", denom: " + denom + "}");
+    }
+    
+    public static int gcdBF(int n, int d)
+    {
+        int gcd = 1;
+        
+        if (n < 0)
+            n = -n;
+        
+        if (d < 0)
+            d = -d;
+        
+        for (int i = 1; i <= n && i <= d; i++)
+            if (n % i == 0 && d % i == 0)
+                gcd = i;
+        
+        return gcd;
+    }
+    
+    public int[] getUncommonDigit()
+    {
+        int[] ary = null;
+        int size;
+        int[] deAryForNum = new int[10];
+        int[] deAryForDenom = new int[10];
+        int tmp;
+        int allDigit;
+        
+        tmp = (num < 0) ? -num : num;
+        do
+        {
+            deAryForNum[tmp % 10] = 1;
+            
+            tmp /= 10;
+        } while(tmp != 0);
+        
+        tmp = (denom < 0) ? -denom : denom;
+        do
+        {
+            deAryForDenom[tmp % 10] = 1;
+            
+            tmp /= 10;
+        } while(tmp != 0);
+        
+        size = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            deAryForNum[i] += deAryForDenom[i];
+            
+            if (deAryForNum[i] == 1)
+            {
+                size++;
+            }
+        }
+        
+        ary = new int [size];
+        allDigit = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            if (deAryForNum[i] == 1)
+            {
+                ary[allDigit] = i;
+                allDigit++;
+            }
+        }
+        return ary;
+    }
+    
+       public FractionFikerW add(FractionFikerW fr)
+    {
+        int resultNum = (this.num) * fr.denom + 
+                (fr.num) * denom;
+        int resultDenom = this.denom * fr.denom;
+        return new FractionFikerW (resultNum, resultDenom);
+    }
+    
+      public FractionFikerW sub(FractionFikerW fr)
+    {
+        int resultNum = (this.num) * fr.denom - 
+                (fr.num) * denom;
+        int resultDenom = this.denom * fr.denom;
+        return new FractionFikerW (resultNum, resultDenom);
+    }
+    
+     public FractionFikerW mul(FractionFikerW fr)
+    {
+        int resultNum = this.num * fr.num;
+        int resultDenom = this.denom * fr.denom;
+        return new FractionFikerW(resultNum, resultDenom);
+    }
+     public FractionFikerW div(FractionFikerW fr)
+    {
+        int resultNum = this.num * fr.denom;
+        int resultDenom = this.denom * fr.num;
+        return new FractionFikerW(resultNum, resultDenom);
+    }
+}
